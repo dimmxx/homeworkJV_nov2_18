75ac5604623f1692c0e5918e7f0e95fd94705e2f
dimmxx <dimmxx@gmail.com>
2018-12-10 19:54:59 +0200

    hw04

--- src/homework/Homework4_23.java
+++ src/homework/Homework4_23.java
@@ -5,7 +5,7 @@ import java.util.Date;
 public class Homework4_23 {
 
     public static void main(String[] args) {
-
+        
         //Homework4_2
         System.out.println();
         System.out.printf("%s%s%s%-15s%s%-20s%s%-7s%s", "|", "#", "|", "currency", "|", "description", "|", "rate", "|");
@@ -19,13 +19,13 @@ public class Homework4_23 {
 
         //Homework4_3
         for(int i = 0; i < 61; i++) {
-            System.out.printf("\r%s %ta %<te %tb  %<tY; %s H:%tH M:%tM S:%tS", "Date:", new Date(), new Date(), "Time:", new Date(), new Date(), new Date());
+            System.out.printf("\rDate: %1$ta %1$<te %1$tb %1$<tY; Time: H:%1$tH M:%1$tM S:%1$tS", new Date());
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-        System.out.println();
+        System.out.println();//Date: Wed 4 Jul 2001; Time: H:19 M:23 S:21
     }
 }
