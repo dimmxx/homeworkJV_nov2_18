9add9a6e79a06417e2e0f19e3a81065286806545
dimmxx <dimmxx@gmail.com>
2018-12-11 18:59:02 +0200

    hw05

---
+++ src/homework/homework5/DemoPowerSupply.java
@@ -0,0 +1,23 @@
+package homework.homework5;
+
+public class DemoPowerSupply {
+
+    public static void main(String[] args) {
+
+        PowerSupply ps = new PowerSupply();
+
+        ps.getinfo();
+
+        ps.switchMode();
+        ps.switchMode();
+        ps.switchMode();
+
+        ps.powerSupplyOn(255);
+
+
+
+
+    }
+
+
+}
---
+++ src/homework/homework5/PowerSupply.java
@@ -0,0 +1,63 @@
+package homework.homework5;
+
+import java.util.Scanner;
+
+public class PowerSupply {
+
+    private boolean state;
+    private boolean mode; //false 5V, true 12V
+    String message;
+    String errorHV = "High voltage. ";
+    String errorLV = "Low voltage. ";
+    String okMessage = "The voltage is normal. ";
+
+    Scanner scan = new Scanner(System.in);
+
+    public void getinfo(){
+        System.out.println("The power supply is " + (state ? "ON. " : "OFF. ")
+                + (message != null ? message : "") + (mode ? "12V " : "5V ")
+                + "mode is selected.");
+    }
+
+    public void switchMode(){
+        if(mode) {
+            mode = false;
+            getinfo();
+        }else {
+            mode = true;
+            getinfo();
+        }
+    }
+
+    public void powerSupplyOn(int input){
+        if (state = checkVoltage(input)){
+            state = true;
+            getinfo();
+            while (state){
+                if (scan.hasNextInt()){
+                    input = scan.nextInt();
+                    state = checkVoltage(input);
+                    getinfo();
+                } else{
+                    System.out.println("Error");
+                    scan.next();
+                }
+            }
+        }else {
+            state = checkVoltage(input);
+            getinfo();
+        }
+    }
+
+    private boolean checkVoltage(int input) {
+        if (input < 200) {
+            message = errorLV;
+            return false;
+        } else if (input > 250) {
+            message = errorHV;
+            return false;
+        }
+        message = okMessage;
+        return true;
+    }
+}
